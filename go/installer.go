a726c68bd20bd52b7f1bff8c1d99fe53265b3845
nirekin <nirekin@hotmail.com>
2018-03-21 09:57:17 +0100

    Use constants from engine

--- go/installer.go
+++ go/installer.go
@@ -7,13 +7,6 @@ import (
 	"github.com/lagoon-platform/engine"
 )
 
-const (
-
-	// The environment variable used to pass the environment descriptor
-	// content to the installer image.
-	starterEnvVariableKey string = "LAGOON_ENV_DESCR"
-)
-
 var (
 	descriptor string
 	logger     *log.Logger
@@ -23,12 +16,11 @@ func main() {
 
 	logger = log.New(os.Stdout, "Lagoon INSTALLER: ", log.Ldate|log.Ltime)
 
-	descriptor = os.Getenv(starterEnvVariableKey)
+	descriptor = os.Getenv(engine.StarterEnvVariableKey)
 	if descriptor == "" {
-		log.Printf(ERROR_REQUIRED_ENV, starterEnvVariableKey)
+		log.Printf(ERROR_REQUIRED_ENV, engine.StarterEnvVariableKey)
 		os.Exit(1)
 	}
-	log.Printf("Ok passed")
 	parseDescriptor(descriptor)
 }
 

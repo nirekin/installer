2b6f0feec638b372269c049e2adcea6db80e4877
Guillaume Barré <nirekin@hotmail.com>
2018-03-22 17:02:19 +0100

    First playbook call (#1)

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apk add --no-cache ansible
 RUN mkdir -p /opt/lagoon/bin
 
 COPY ./go/installer /opt/lagoon/bin/installer
+COPY ./ansible/ /opt/lagoon/ansible
 
 ENTRYPOINT ["/opt/lagoon/bin/installer"]
 
--- go/installer.go
+++ go/installer.go
@@ -1,72 +1,110 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"io/ioutil"
-	_ "path/filepath"
 
 	"github.com/lagoon-platform/engine"
 )
 
 var (
 	descriptor string
-	logger     *log.Logger
+	loggerLog  *log.Logger
+	loggerErr  *log.Logger
+	lagoon     engine.Lagoon
 )
 
 func main() {
 
-	logger = log.New(os.Stdout, "Lagoon INSTALLER: ", log.Ldate|log.Ltime)
+	loggerLog = log.New(os.Stdout, "Lagoon INSTALLER LOG: ", log.Ldate|log.Ltime)
+	loggerErr = log.New(os.Stderr, "Lagoon INSTALLER ERROR: ", log.Ldate|log.Ltime)
 
-	descriptor = os.Getenv(engine.StarterEnvVariableKey)
-	if descriptor == "" {
-		logger.Printf(ERROR_REQUIRED_ENV, engine.StarterEnvVariableKey)
-		os.Exit(1)
-	}
-	parseDescriptor(descriptor)
+	checkProxy()
+	checkDescriptor()
+	checkPlaybooks()
+}
 
-	if os.Getenv("http_proxy") == "" {
-		log.Fatal(fmt.Errorf(ERROR_REQUIRED_ENV, "http_proxy"))
-	}
-	logger.Printf("Using http proxy", os.Getenv("http_proxy"))
-	if os.Getenv("https_proxy") == "" {
-		log.Fatal(fmt.Errorf(ERROR_REQUIRED_ENV, "https_proxy"))
-	}
-	logger.Printf("Using https proxy", os.Getenv("https_proxy"))
-	fileName := "container_output.json"
-	f, err := os.Create(fileName)
+func checkPlaybooks() {
+	files, err := ioutil.ReadDir("/opt/lagoon/ansible")
 	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	_, err = f.Write([]byte("written from the go code running on the container"))
-	if err != nil {
-		panic(err)
-	}
-	files, err := ioutil.ReadDir("./")
-	if err != nil {
-		log.Fatal(err)
+		loggerErr.Fatal(err)
 	}
 
 	for _, f := range files {
-		logger.Println(f.Name())
+		loggerLog.Println(f.Name())
 	}
+
+	if _, err := os.Stat("/opt/lagoon/ansible/HelloWorld.yml"); os.IsNotExist(err) {
+		loggerErr.Fatalf("/opt/lagoon/ansible/HelloWorld.yml is missing")
+	}
+
+	loggerLog.Println("starting playbook 1")
+	cmd := exec.Command("ansible-playbook", "HelloWorld.yml")
+	cmd.Dir = "/opt/lagoon/ansible/"
+
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		loggerErr.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			loggerLog.Printf("%s\n", scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		loggerErr.Fatal(err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		loggerErr.Fatal(err)
+	}
+
+	if _, err := os.Stat("/tmp/testfile.txt"); os.IsNotExist(err) {
+		loggerErr.Fatalf("/tmp/testfile.txt has not been created")
+	}
+
+	dat, err := ioutil.ReadFile("/tmp/testfile.txt")
+	if err != nil {
+		loggerErr.Fatal(err)
+	}
+	loggerLog.Println("------ testfile.txt - content - starts ------")
+	loggerLog.Println(string(dat))
+	loggerLog.Println("------ testfile.txt - content - ens ------")
+
 }
 
-func parseDescriptor(descriptor string) ([]byte, error) {
-	logger.Printf(LOG_PARSING)
-
-	lagoon, e := engine.CreateFromContent(logger, []byte(descriptor))
-	if e != nil {
-		return nil, e
+func checkDescriptor() {
+	descriptor = os.Getenv(engine.StarterEnvVariableKey)
+	if descriptor == "" {
+		loggerErr.Fatalf(ERROR_REQUIRED_ENV, engine.StarterEnvVariableKey)
 	}
-
-	content, err := lagoon.GetContent()
+	var err error
+	lagoon, err = engine.CreateFromContent(loggerLog, []byte(descriptor))
 	if err != nil {
-		return nil, err
+		loggerErr.Fatalf(ERROR_PARSING_DESCRIPTOR, err.Error())
 	}
-	return content, nil
+	loggerLog.Printf("Number of providers to process : %d", lagoon.GetEnvironment().GetProviderDescriptions().Count())
+	loggerLog.Printf("Number of nodes to create: %d", lagoon.GetEnvironment().GetNodeDescriptions().Count())
+}
+
+func checkProxy() {
+	if os.Getenv("http_proxy") == "" {
+		loggerErr.Fatal(fmt.Errorf(ERROR_REQUIRED_ENV, "http_proxy"))
+
+	}
+	if os.Getenv("https_proxy") == "" {
+		loggerErr.Fatal(fmt.Errorf(ERROR_REQUIRED_ENV, "https_proxy"))
+	}
+	loggerLog.Printf("Using https proxy %s", os.Getenv("https_proxy"))
+	loggerLog.Printf("Using http proxy %s", os.Getenv("http_proxy"))
 }

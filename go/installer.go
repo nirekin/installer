7c638352fce6f97c414f45fa66222b4d9c3401eb
nirekin <nirekin@hotmail.com>
2018-06-14 08:59:21 +0200

    Add a sub folder for provider, and use components in lagoon-platform

--- go/installer.go
+++ go/installer.go
@@ -46,6 +46,7 @@ func main() {
 }
 
 func run() (e error) {
+	// Check if the received action is supporter by the engine
 	a := os.Getenv(engine.ActionEnvVariableKey)
 	switch a {
 	case engine.ActionCreate.String():
@@ -61,7 +62,9 @@ func run() (e error) {
 	return
 }
 
+// runCreate launches the envinronemt creation
 func runCreate() (e error) {
+	// Stack of functions required to create an envinronemnt
 	calls := []func() error{
 		fproxy,
 		fclient,
@@ -78,7 +81,9 @@ func runCreate() (e error) {
 	return
 }
 
+// runCheck launches the envinronemt check
 func runCheck() (e error) {
+	// Stack of functions required to check an envinronemnt
 	calls := []func() error{
 		fproxy,
 		fexchangeFoldef,
@@ -93,6 +98,7 @@ func runCheck() (e error) {
 	return
 }
 
+//launch runs a slice of functions
 func launch(fs []func() error) error {
 	for _, f := range fs {
 		e := f()
@@ -107,6 +113,11 @@ func fcreate() error {
 	// Check if a session already exists
 	var createSession engine.CreationSession
 	var d string
+	providerFolders, err := enrichExchangeFolder(ef, lagoon.Environment())
+
+	if err != nil {
+		return (err)
+	}
 
 	b, s := engine.HasCreationSession(ef)
 	if !b {
@@ -117,39 +128,47 @@ func fcreate() error {
 
 	for _, n := range lagoon.Environment().NodeSets {
 		loggerLog.Printf(LOG_PROCESSING_NODE, n.Name)
+		p := n.Provider.ProviderName()
 
 		if val, ok := createSession.Uids[n.Name]; ok {
 			loggerLog.Printf(LOG_REUSING_UID_FOR_CLIENT, val, client, n.Name)
-			d = path.Join(engine.InstallerVolume, val)
+			d = path.Join(providerFolders[p].Input.Path(), val)
 		} else {
 			uid := engine.GetUId()
 			loggerLog.Printf(LOG_CREATING_UID_FOR_CLIENT, uid, client, n.Name)
+			d = path.Join(providerFolders[p].Input.Path(), uid)
 
-			b, e := n.ExtraVars(client, uid, engine.InstallerVolume)
+			din := path.Join(d, "input")
+
+			//TODO Find a sexy way to pass the output folder to the container
+			// avoiding using the config file
+			b, e := n.ExtraVars(client, uid, providerFolders[p].Output.Path())
 			if e != nil {
 				return (e)
 			}
 			createSession.Add(n.Name, uid)
 
-			d = path.Join(engine.InstallerVolume, uid)
-			engine.SaveFile(loggerLog, d, engine.NodeConfigFileName, b)
+			engine.SaveFile(loggerLog, din, engine.NodeConfigFileName, b)
 
 			b, e = n.DockerVars()
 			if e != nil {
 				return (e)
 			}
-			engine.SaveFile(loggerLog, d, engine.NodeDockerFileName, b)
+			engine.SaveFile(loggerLog, din, engine.NodeDockerFileName, b)
 
 			if httpProxy != "" {
-				engine.SaveProxy(loggerLog, d, httpProxy, httpsProxy, noProxy)
+				engine.SaveProxy(loggerLog, din, httpProxy, httpsProxy, noProxy)
 			}
 		}
 
 		// TODO REMOVE HARDCODED STUFF AND BASED THIS ON THE RECEIVED ENV FILE
 		os.Setenv("ANSIBLE_INVENTORY", "/opt/lagoon/ansible/aws-provider/scripts/ec2.py")
 		os.Setenv("EC2_INI_PATH", "/opt/lagoon/ansible/aws-provider/scripts/ec2.ini")
+		os.Setenv("PROVIDER_PATH", "/opt/lagoon/ansible/aws-provider")
+
 		os.Setenv("http_proxy", httpProxy)
 		os.Setenv("https_proxy", httpsProxy)
+
 		// TODO ENV VARIABLES SHOULD BE PASSED TO THE PLAYBOOK LAUNCHER
 
 		//i := 1
@@ -184,9 +203,7 @@ func flogLagoon() error {
 		if !ok {
 			return fmt.Errorf(ERROR_PARSING_ENVIRONMENT, ve.Error())
 		} else {
-
 			loggerLog.Printf(ve.Error())
-
 			b, e := vErrs.JSonContent()
 			if e != nil {
 				return fmt.Errorf(ERROR_GENERIC, e)
@@ -207,7 +224,7 @@ func flogLagoon() error {
 
 func fexchangeFoldef() error {
 	var err error
-	ef, err = engine.ClientExchangeFolder(engine.InstallerVolume, "")
+	ef, err = engine.CreateExchangeFolder(engine.InstallerVolume, "")
 	if err != nil {
 		return fmt.Errorf(ERROR_CREATING_EXCHANGE_FOLDER, engine.ClientEnvVariableKey)
 	}
@@ -251,4 +268,27 @@ func ffailOnLagoonError() error {
 		return fmt.Errorf(ERROR_PARSING_DESCRIPTOR, lagoonError.Error())
 	}
 	return nil
+}
+
+//enrichExchangeFolder adds a sub level of ExchangeFolder to the received one.
+//This will add a "sub ExchangeFolder" for each provider willing to be in charge
+// of a nodeset creation.
+func enrichExchangeFolder(f engine.ExchangeFolder, e model.Environment) (r map[string]engine.ExchangeFolder, err error) {
+	r = make(map[string]engine.ExchangeFolder)
+	for _, n := range lagoon.Environment().NodeSets {
+		p := n.Provider.ProviderName()
+		if !f.Input.Contains(p) {
+			var pEf engine.ExchangeFolder
+			pEf, err = engine.CreateExchangeFolder(f.Input.Path(), p)
+			if err != nil {
+				return
+			}
+			err = pEf.Create()
+			if err != nil {
+				return
+			}
+			r[p] = pEf
+		}
+	}
+	return
 }
